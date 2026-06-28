ac679dccaaa5c2b81f3d6cf86bbeb88c8610cd16
Tarang Suryawanshi <tarangsuryawanshitarang@gmail.com>
2026-06-28 21:06:30 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[')
+                st.push(ch);
+
+            else {
+                if (st.empty()) return false;
+
+                if (ch == ')' && st.top() != '(') return false;
+                if (ch == ']' && st.top() != '[') return false;
+                if (ch == '}' && st.top() != '{') return false;
+
+                // Pop the matched opening bracket
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
